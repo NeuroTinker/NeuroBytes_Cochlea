56f188e08bdee3d7fd10e083b5285354f980227e
jrwhite <jrwhite20@gmail.com>
2017-06-07 14:37:34 -0500

    added uart, led pwm, and calibration

--- FIRMWARE/HAL.h
+++ FIRMWARE/HAL.h
@@ -8,6 +8,7 @@
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/exti.h>
 #include <libopencm3/stm32/adc.h>
+#include <libopencm3/stm32/usart.h>
 
 #include "kiss_fft.h"
 
@@ -18,7 +19,7 @@
 #define UPDATE_TIME     50000  // microseconds update time 50 Hz = 10 main ticks
 #define UPDATE_TICKS    = UPDATE_TIME / MAIN_TICK_TIME
 #define SAMPLE_TIME     100 // sample every 100 us, 10 kHz
-#define NUM_SAMPLES     UPDATE_TIME / SAMPLE_TIME
+#define NUM_SAMPLES     2048
 
 #define PORT_MIC    GPIOA
 #define PIN_MIC     GPIO1
@@ -30,9 +31,11 @@
 #define PIN_LED1    GPIO15
 
 extern volatile uint8_t main_tick;
-extern volatile uint8_t tick;
+extern volatile uint16_t tick;
 extern kiss_fft_cpx timedata[N]; // time domain samples
 extern volatile uint8_t data_ready_flag;
+
+static const uint16_t gamma_lookup[1024];
 
 void systick_setup(int ums);
 void clock_setup(void);
@@ -40,6 +43,9 @@ void gpio_setup(void);
 void tim_setup(void);
 void adc_setup(void);
 uint32_t read_adc(void);
+void usart_setup(void);
+void tim_setup(void);
+void setLED(uint32_t led1, uint32_t led2, uint32_t led3, uint32_t led4);
 
 
 #endif
--- FIRMWARE/main.c
+++ FIRMWARE/main.c
@@ -7,8 +7,12 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
+#include "mini-printf.h"
+
 #include "kiss_fft.h"
 #include "HAL.h"
+
+//#define PRINT
 
 
 
@@ -20,61 +24,67 @@ int main()
     systick_setup(100);
     gpio_setup();
     adc_setup();
+    usart_setup();
+    tim_setup();
+
+    char strDisp[20];
+	for (i=0;i<20;i++)
+	{
+		strDisp[i] = 0;
+	}
+	uint16_t val;
+    uint32_t bucket[5] = {0,0,0,0};
+    uint16_t bucket_end[4] = {100, 200, 300, 400};
+    uint8_t bucket_i = 0;
+    uint32_t smallest_bucket_i =0;
 
     kiss_fft_cpx freqdata[N]; // frequency domain samples
     // initialize FFT buffer
     kiss_fft_cfg fft_buffer = kiss_fft_alloc(NUM_BUCKETS, 0, 0, 0);
 
     uint32_t output_bucket[4];
-
     for(;;){
         if (main_tick == 1){
             main_tick = 0;
             if (data_ready_flag == 1){
                 kiss_fft(fft_buffer, timedata, freqdata); // FFT transform time domain -> frequency domain
                 data_ready_flag = 0;
-                for(i=100; i<500; i++){
-                    if (freqdata[i].r >= freqdata[largest_bucket].r){
-                        largest_bucket = i;
-                    }
-                    /*
-                    if (freqdata[i].r != 0){
-                        gpio_set(PORT_LED, PIN_LED3);
-                    } else if (freqdata[i].r == 0){
-                        gpio_set(PORT_LED, PIN_LED1);
-                    }
-                    */
-                    gpio_clear(PORT_LED, PIN_LED1);
-                    gpio_clear(PORT_LED, PIN_LED2);
-                    gpio_clear(PORT_LED, PIN_LED3);
-                    gpio_clear(PORT_LED, PIN_LED4);
-                    
-                    /*
-                    if (largest_bucket < NYQUIST_F / NUM_OUTPUTS){
-                        gpio_set(PORT_LED, PIN_LED1);
-                    } else if (largest_bucket < 2*(NYQUIST_F / NUM_OUTPUTS)){
-                        gpio_set(PORT_LED, PIN_LED2);
-                    } else if (largest_bucket < 3*(NYQUIST_F / NUM_OUTPUTS)){
-                        gpio_set(PORT_LED, PIN_LED3);
-                    } else if (largest_bucket < 4*(NYQUIST_F / NUM_OUTPUTS)){
-                        gpio_set(PORT_LED, PIN_LED4);
-                    }
-                    */
-
-                    if (largest_bucket < 200){
-                        gpio_set(PORT_LED, PIN_LED1);
-                    } else if (largest_bucket < 600){
-                        gpio_set(PORT_LED, PIN_LED2);
-                    } else if (largest_bucket < 1000){
-                        gpio_set(PORT_LED, PIN_LED3);
-                    } else if (largest_bucket < 1400){
-                        gpio_set(PORT_LED, PIN_LED4);
+                for(i=0; i<5; i++){
+                    bucket[i] = 0;
+                }
+                bucket_i = 0;
+                for(i=1; i<500; i++){
+                    val = freqdata[i].r;
+                    #ifdef PRINT
+                    mini_snprintf(strDisp, 20, "%u",val);
+		            usart_print(strDisp);
+                    usart_send_blocking(USART2, '\n');
+                    #endif
+                    if (i<bucket_end[bucket_i]){
+                        bucket[bucket_i] += val;
+                    } else{
+                        bucket[bucket_i] = bucket[bucket_i] / 200;
+                        if (bucket_i != 4){
+                            bucket_i += 1;
+                        } else{
+                            break;
+                        }
                     }
                 }
-                
-                
             }
-            
+            smallest_bucket_i = 0;
+            for(i=0; i<4; i++){
+                if (bucket[i] < bucket[smallest_bucket_i]){
+                    smallest_bucket_i = i;
+                }
+            }
+            for(i=0; i<4; i++){
+                bucket[i] -= bucket[smallest_bucket_i];
+                mini_snprintf(strDisp, 20, "%u",bucket[i]);
+		            usart_print(strDisp);
+                    usart_send_blocking(USART2, '\n');
+            }
+            setLED(bucket[0],bucket[1],bucket[2],bucket[3]);
         }
     }
 }
